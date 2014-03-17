446ad03ca68389b7860f2a61e6b68e4361d855cb
Aloisio Ribeiro <aloisio@mobstats.com>
2014-03-17 01:40:07 +0000

    Responding to Action Menu

--- gen/android/v2/R.java
+++ gen/android/v2/R.java
@@ -1205,6 +1205,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050027;
         public static final int action_menu_presenter=0x7f050028;
         public static final int action_mode_close_button=0x7f050035;
+        public static final int action_settings=0x7f050054;
         public static final int activity_chooser_view_content=0x7f050036;
         public static final int alignBounds=0x7f050002;
         public static final int alignMargins=0x7f050003;
@@ -1324,11 +1325,21 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int activity_display_message=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_theme_color=0x7f030019;
+        public static final int activity_theme_dark=0x7f03001a;
+        public static final int activity_theme_dark_light=0x7f03001b;
+        public static final int activity_theme_image=0x7f03001c;
+        public static final int activity_theme_light=0x7f03001d;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001e;
     }
     public static final class menu {
         public static final int menu_display_message=0x7f0c0000;
         public static final int menu_main=0x7f0c0001;
+        public static final int theme_color=0x7f0c0002;
+        public static final int theme_dark=0x7f0c0003;
+        public static final int theme_dark_light=0x7f0c0004;
+        public static final int theme_image=0x7f0c0005;
+        public static final int theme_light=0x7f0c0006;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1377,11 +1388,18 @@ containing a value of this type.
         public static final int action_ThemeImage=0x7f0a0013;
         public static final int action_ThemeLight=0x7f0a0010;
         public static final int action_help=0x7f0a000e;
+        public static final int action_settings=0x7f0a0019;
         public static final int app_name=0x7f0a000d;
         public static final int button_send=0x7f0a0016;
         public static final int edit_message=0x7f0a0015;
         public static final int hello_android=0x7f0a0014;
+        public static final int hello_world=0x7f0a001a;
         public static final int title_activity_display_message=0x7f0a0017;
+        public static final int title_activity_theme_color=0x7f0a001d;
+        public static final int title_activity_theme_dark=0x7f0a0018;
+        public static final int title_activity_theme_dark_light=0x7f0a001c;
+        public static final int title_activity_theme_image=0x7f0a001e;
+        public static final int title_activity_theme_light=0x7f0a001b;
     }
     public static final class style {
         /** 
--- src/android/v2/MainActivity.java
+++ src/android/v2/MainActivity.java
@@ -2,10 +2,12 @@ package android.v2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
-import android.support.v7.app.ActionBarActivity;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -22,8 +24,72 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+
+    	// Eclipse's code
+//    	getMenuInflater().inflate(R.menu.menu_main, menu);
+//      return true;
+    	
+    	// Android's page code
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.menu.menu_main, menu);
+    	return super.onCreateOptionsMenu(menu);
+    }
+
+    
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	// Handle presses on the action bar items
+    	switch(item.getItemId()) {
+    		case R.id.action_help:
+//    			fOpenHelp();
+    			return true;
+    		case R.id.action_ThemeDark:
+    			fOpenDarkThemeActivity();
+    			return true;
+    		case R.id.action_ThemeLight:
+    			fOpenLightThemeActivity();
+    			return true;
+    		case R.id.action_ThemeDarkLight:
+    			fOpenDarkLightThemeActivity();
+    			return true;
+    		case R.id.action_ThemeColor:
+    			fOpenColorThemeActivity();
+    			return true;
+    		case R.id.action_ThemeImage:
+    			fOpenImageThemeActivity();
+    			return true;
+    		default:
+    			return super.onOptionsItemSelected(item);
+    	}
+    }
+
+    
+    public void fOpenDarkThemeActivity() {
+    	Intent intent = new Intent(this, ThemeDarkActivity.class);
+    	startActivity(intent);
+    }
+    
+    
+    public void fOpenLightThemeActivity() {
+    	Intent intent = new Intent(this, ThemeLightActivity.class);
+    	startActivity(intent);
+    }
+    
+    
+    public void fOpenDarkLightThemeActivity() {
+    	Intent intent = new Intent(this, ThemeDarkLightActivity.class);
+    	startActivity(intent);
+    }
+    
+    
+    public void fOpenColorThemeActivity() {
+    	Intent intent = new Intent(this, ThemeColorActivity.class);
+    	startActivity(intent);
+    }
+    
+    
+    public void fOpenImageThemeActivity() {
+    	Intent intent = new Intent(this, ThemeImageActivity.class);
+    	startActivity(intent);
     }
     
     
